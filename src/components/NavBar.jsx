f3fb7d0d526eb5da03edf7e377c3c40a7b54ee27
Edgar Rincon <eers222@gmail.com>
2023-02-12 23:32:42 -0600

    Reformatear NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,43 +2,25 @@ import logo from '../assets/images/logo.png';
 import { AiFillHome, AiFillBook } from 'react-icons/ai';
 import { BsFillPersonFill, BsFillBookmarkFill, BsFillCartFill } from 'react-icons/bs';
 import '../assets/styles/NavBar.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-    <nav className='navbar navbar-expand navbar-dark bg-dark sticky-top'>
-        <div className='container'>
-            <img src={logo} alt="SI-Commerce logo" className='logo'></img>
-            <div id="navbar">
-                <ul className="navbar-nav ml-auto">
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            <AiFillHome /> Home
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            <AiFillBook /> Catálogo
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            <BsFillPersonFill /> Mi Perfil
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            <BsFillBookmarkFill /> Mis guardados
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            <BsFillCartFill /> Carrito
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <Navbar expand="lg" bg="primary">
+        <Container>
+            <Navbar.Brand>
+                <img src={logo} alt="SI-Commerce logo" className='logo'></img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link> <AiFillHome />Home</Nav.Link>
+            <Nav.Link><AiFillBook /> Catálogo</Nav.Link>
+            <Nav.Link><BsFillPersonFill /> Mi Perfil</Nav.Link>
+            <Nav.Link><BsFillBookmarkFill /> Mis guardados</Nav.Link>
+            <Nav.Link><BsFillCartFill /> Carrito</Nav.Link>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
     );
 }
 
